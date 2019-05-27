47a9c044dc06d1d90364a985f34b29cba96d782c
Lubomir Rintel <lkundrak@v3.sk>
2019-05-27 22:39:04 +0200

    test/BONDBRIDGETEAMVLAN: don't fsck redundantly
    There's a fsck line in test.sh just prior to run-qemu.

--- test/TEST-70-BONDBRIDGETEAMVLAN/test.sh
+++ test/TEST-70-BONDBRIDGETEAMVLAN/test.sh
@@ -31,7 +31,7 @@ run_server() {
         ${SERIAL:--serial file:"$TESTDIR"/server.log} \
         -watchdog i6300esb -watchdog-action poweroff \
         -no-reboot \
-        -append "panic=1 loglevel=7 root=/dev/sda rootfstype=ext3 rw console=ttyS0,115200n81 selinux=0 rd.debug" \
+        -append "panic=1 loglevel=7 root=/dev/sda rootfstype=ext3 rw console=ttyS0,115200n81 selinux=0 rd.debug rd.skipfsck" \
         -initrd "$TESTDIR"/initramfs.server \
         -pidfile "$TESTDIR"/server.pid -daemonize || return 1
     chmod 644 -- "$TESTDIR"/server.pid || return 1
