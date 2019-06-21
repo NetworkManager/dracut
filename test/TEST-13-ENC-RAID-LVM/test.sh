f30a9a34b4bb14dab7d794c6e4ffb452cd48c093
Lubomir Rintel <lkundrak@v3.sk>
2019-06-21 13:02:22 +0200

    Revert "extralog"
    This reverts commit c728a03ab7757ad7cc1e8fd9e575e5544565c2ef.

--- test/TEST-13-ENC-RAID-LVM/test.sh
+++ test/TEST-13-ENC-RAID-LVM/test.sh
@@ -95,7 +95,7 @@ test_setup() {
     (
 	export initdir=$TESTDIR/overlay
 	. $basedir/dracut-init.sh
-	inst_multiple sfdisk mke2fs poweroff cp umount grep du df find xargs
+	inst_multiple sfdisk mke2fs poweroff cp umount grep
 	inst_hook initqueue 01 ./create-root.sh
         inst_hook initqueue/finished 01 ./finished-false.sh
 	inst_simple ./99-idesymlinks.rules /etc/udev/rules.d/99-idesymlinks.rules
