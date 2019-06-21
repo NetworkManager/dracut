4e20621e0a61915255e3bbfb863c305aee2ed96b
root <root@fedora31-1.local>
2019-06-21 11:03:20 +0200

    extralog13

--- test/TEST-13-ENC-RAID-LVM/create-root.sh
+++ test/TEST-13-ENC-RAID-LVM/create-root.sh
@@ -32,6 +32,9 @@ lvm vgchange -ay && \
 mke2fs /dev/dracut/root && \
 mkdir -p /sysroot && \
 mount /dev/dracut/root /sysroot && \
+find /source -type f |xargs du -sh
+du -sh /source /source/*
+df -h /source /sysroot
 cp -a -t /sysroot /source/* && \
 umount /sysroot && \
 sleep 2 && \
